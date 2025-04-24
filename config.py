dc05e4236cf911bd07e27312a9a0154161bdaa20
gorobarak <gorobark@gmail.com>
2025-04-24 13:42:18 +0300

    organzing the config parameters

--- config.py
+++ config.py
@@ -4,37 +4,49 @@ import torch
 def get_default_cfg():
     default_cfg = {
         "seed": 49,
+
+        # Training 
         "batch_size": 4096,
         "lr": 3e-4,
         "num_tokens": int(1e9),
-        "l1_coeff": 0,
         "beta1": 0.9,
         "beta2": 0.99,
         "max_grad_norm": 100000,
         "seq_len": 128,
         "dtype": torch.float32,
+
+        # Model 
         "model_name": "gpt2-small",
         "site": "resid_pre",
         "layer": 8,
         "act_size": 768,
-        "dict_size": 12288,
-        "device": "cuda:0",
         "model_batch_size": 512,
         "num_batches_in_buffer": 10,
-        "dataset_path": "Skylion007/openwebtext",
-        "wandb_project": "sparse_autoencoders",
-        "input_unit_norm": True,
-        "perf_log_freq": 1000,
+
+        # SAE 
         "sae_type": "topk",
-        "checkpoint_freq": 10000,
+        "dict_size": 12288,
+        "device": "cuda",
+        "input_unit_norm": True,
         "n_batches_to_dead": 5,
+        
+        # Dataset 
+        "dataset_path": "Skylion007/openwebtext",
+        
+        # Logging
+        "wandb_project": "sparse_autoencoders",
+        "perf_log_freq": 1000,
+        "checkpoint_freq": 10000,
+        
 
         # (Batch)TopKSAE specific
         "top_k": 32,
         "top_k_aux": 512,
         "aux_penalty": (1/32),
-        # for jumprelu
+        
+        # JumpRelu specific
         "bandwidth": 0.001,
+        "l1_coeff": 0,
     }
     default_cfg = post_init_cfg(default_cfg)
     return default_cfg
