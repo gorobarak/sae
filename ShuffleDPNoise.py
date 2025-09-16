15101f8835cd7336ee366757e2b92bdd3f04d527
gorobarak <gorobark@gmail.com>
2025-09-16 10:55:33 +0300

    fix sensitivity not usage in shuffleDP

--- ShuffleDPNoise.py
+++ ShuffleDPNoise.py
@@ -11,18 +11,24 @@ class ShuffledDPSanitizer:
 
   def sanitize_counter_array(self, counter_array, n, sensitivity, epsilon):
     """
-    Args:
-        counter_array: array of non-negative integers.
-        n:  number of records (users) represented in the counter array.
-        sensitivity: how many entries each user at most contributed to.
-        epsilon: input (not output!) privacy parameter.
+    counter_array: array of non-negative integers.
+    n:  number of records (users) represented in the counter array.
+        Note: This parameter is currently not used in the code.
+    sensitivity: how many entries each user at most contributed to.
+        If set to 0, we assume that each user contributed to all entries.
+    epsilon: input (not output!) privacy parameter.
     """
-    # Set privacy parameters
     num_counters = counter_array.size(0)
-    epsilon_per_counter = epsilon / num_counters
-    delta_per_counter = self.delta0 / num_counters
-    output_epsilon = epsilon_per_counter * (np.e ** epsilon_per_counter - 1) * num_counters + \
-                        epsilon_per_counter * (2 * num_counters * np.log(1. / self.delta_prime_composition)) ** 0.5
+    # Set privacy parameters without sparsity
+    if sensitivity == 0:
+      epsilon_per_counter = epsilon / num_counters
+      delta_per_counter = self.delta0 / num_counters
+      output_epsilon = epsilon_per_counter * (np.e ** epsilon_per_counter - 1) * num_counters + \
+                          epsilon_per_counter * (2 * num_counters * np.log(1. / self.delta_prime_composition)) ** 0.5
+    else:
+       epsilon_per_counter = epsilon / sensitivity
+       delta_per_counter = self.delta0 / sensitivity
+       output_epsilon = epsilon
 
     # Impose privacy noise
     negbin_param_r = 1.
@@ -37,19 +43,24 @@ class ShuffledDPSanitizer:
 
     return noisy_counter_array,output_epsilon
 
+
+
 class ShuffledDPHistogramSanitizer:
 
   def __init__(self):
     self.shuffled_dp_sanitizer = ShuffledDPSanitizer()
 
-  def sanitize(self, histogram, n, k, epsilon):
+  def sanitize(self, histogram, n, k, epsilon, use_sparsity=True):
     """
     histogram: input histogram.
     n:  number of records (users) represented in the counter array.
     k: how many histogram entries each user at most contributed to.
     epsilon: input (not output!) privacy parameter.
     """
-    return self.shuffled_dp_sanitizer.sanitize_counter_array(histogram, n, k, epsilon)
+    if use_sparsity:
+      return self.shuffled_dp_sanitizer.sanitize_counter_array(histogram, n, k, epsilon)
+    else:
+      return self.shuffled_dp_sanitizer.sanitize_counter_array(histogram, n, 0, epsilon)
 
 
 class ShuffledDPMeanSanitizer:
@@ -75,7 +86,7 @@ class ShuffledDPMeanSanitizer:
     """
     noisy_counter_array, output_epsilon = self.shuffled_dp_sanitizer.sanitize_counter_array(self.counter_array,
                                                                                             self.n,
-                                                                                            self.d,
+                                                                                            0,
                                                                                             epsilon)
     noisy_mean_array = 2 * noisy_counter_array / self.n - 1
     return noisy_mean_array, output_epsilon
