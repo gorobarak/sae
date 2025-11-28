b95de96dda30c836f84a4610a327b9de8a0fd778
gorobarak <gorobark@gmail.com>
2025-11-28 17:02:16 +0200

    add conversation column to preprocessing

--- preprocess.py
+++ preprocess.py
@@ -22,7 +22,7 @@ def preprocess(dataset_name: str, model_name: str, L_max: int = 256):
                                                padding=False, # no padding 
                                                truncation=False, # no truncation 
                                                return_tensors=None) # return as list of token ids
-        return {"input_ids": tokens, "length": len(tokens)}
+        return {"input_ids": tokens, "length": len(tokens), "conversation": new_conv}
     
     print("tokenizing only the initial user message", file=os.sys.stderr)
     tokenized_dataset = dataset.map(tokenize_example)
@@ -30,8 +30,8 @@ def preprocess(dataset_name: str, model_name: str, L_max: int = 256):
     print(f"Filtering examples longer than {L_max} tokens", file=os.sys.stderr)
     filtered_dataset = tokenized_dataset.filter(lambda x: x["length"] < L_max)
 
-    # remove all columns except input_ids and length
-    filtered_dataset = filtered_dataset.remove_columns([col for col in filtered_dataset.column_names if col not in ["input_ids", "length"]])
+    # remove all columns except input_ids, length and conversation
+    filtered_dataset = filtered_dataset.remove_columns([col for col in filtered_dataset.column_names if col not in ["input_ids", "length", "conversation"]])
 
     # save to disk
     path = f"data/preprocessed/{dataset_name.replace('/', '_')}/{model_name.replace('/', '_')+f"_L={L_max}"}"
@@ -42,6 +42,6 @@ def preprocess(dataset_name: str, model_name: str, L_max: int = 256):
 
 if __name__ == "__main__":
     disable_progress_bar()
-    for model_name in ["microsoft/Phi-3-mini-4k-instruct", "mistralai/Mistral-7B-Instruct-v0.1"]:
+    for model_name in ["meta-llama/Llama-3.1-8B", "Qwen/Qwen2.5-7B-Instruct", "mistralai/Mistral-7B-Instruct-v0.1", "microsoft/Phi-3-mini-4k-instruct"]:
         print(f"Preprocessing for model: {model_name}", file=os.sys.stderr)
         preprocess("allenai/WildChat-1M", model_name, L_max=256)
