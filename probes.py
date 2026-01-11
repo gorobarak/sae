33399bf4a671f74f5e1b4e8a164678cac7c07c68
gorobarak <gorobark@gmail.com>
2026-01-11 13:26:38 +0200

    sync

--- probes.py
+++ probes.py
@@ -254,9 +254,12 @@ def create_datasets(
 
     model = model.to("cuda")
 
-    # workaround for HF Transformers output_hidden_states=True outputing the final layer output after the final LN
+    # workaround for HF Transformers output_hidden_states=True cacheing the final layer hidden state after the final LN which is not a part of the residual stream
+    # Norm module is the final LN in modern HF transformers models see Qwen2Model for example https://github.com/huggingface/transformers/blob/main/src/transformers/models/qwen2/modeling_qwen2.py#L340 
+    if not hasattr(model.model, "norm"):
+        raise ValueError("HF WORKAROUND: Could not find final norm module in model")
     identity = Identity()
-    model.model.norm = identity
+    final_ln_module = model.model.norm
 
     # load dataset
     dataset = load_from_disk(
@@ -282,6 +285,9 @@ def create_datasets(
     for i in range(num_iterations):
         inputs = next(data_loader_iter)
         inputs = {key: val.to("cuda") for key, val in inputs.items()}
+        
+        # forward pass for hidden states
+        model.model.norm = identity  # remove final LN for residual stream extraction
         with torch.no_grad():
             output = model(**inputs, output_hidden_states=True)
             hidden_states = output.hidden_states[
@@ -299,13 +305,15 @@ def create_datasets(
                     acts_to_save.cpu()
                 )  # [batch, d_model]
 
-        # # generate responses
-        # responses = generate(**inputs, model=model, eos_token_id=tokenizer.eos_token_id)  # [batch, gen_len]
-        # Ys["response_token_ids"].append(responses.cpu())
+        # generate responses
+        # restore the final LN
+        model.model.norm = final_ln_module
+        responses = generate(**inputs, model=model, eos_token_id=tokenizer.eos_token_id)  # [batch, gen_len]
+        Ys["response_token_ids"].append(responses.cpu())
 
-        # # record length
-        # lengths = compute_response_length(responses, tokenizer.eos_token_id)  # [batch]
-        # Ys["lengths"].append(lengths.cpu())
+        # record length
+        lengths = compute_response_length(responses, tokenizer.eos_token_id)  # [batch]
+        Ys["lengths"].append(lengths.cpu())
 
         print(f"Processed batch {i + 1}/{num_iterations}", file=sys.stderr)
 
